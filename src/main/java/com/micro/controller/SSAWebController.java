b1d702d75fb21a95e86f9f5caf3c028db6cc0755
gdpatil39 <gdpatil39@gmail.com>
2025-04-03 10:33:14 +0530

    featurebrach first

--- src/main/java/com/micro/controller/SSAWebController.java
+++ src/main/java/com/micro/controller/SSAWebController.java
@@ -21,7 +21,7 @@ public class SSAWebController {
 		if (String.valueOf(ssn).length()!=9)
 		{ logger.warn("Invalid SSN received: {}", ssn);
 			return new ResponseEntity<>("Invalid",HttpStatus.BAD_REQUEST);}
-		
+		// this change from Gaurav side
 		int stateCode=ssn%10;
 		 String stateName = switch (stateCode) {
 	        case 1 -> "WashingtonDC";
