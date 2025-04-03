e20f2ccc98a9cd9322e34ab3a94903b2dfe8316c
Jagdishchandradive <jagdishchandradive@gmail.com>
2025-04-03 10:05:33 +0530

    second commit

--- src/main/java/com/micro/controller/SSAWebController.java
+++ src/main/java/com/micro/controller/SSAWebController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SSAWebController {
 	private static final Logger logger = LoggerFactory.getLogger(SSAWebController.class);
 	
+	
+	
 	@GetMapping("/find/{ssn}")
 	public ResponseEntity<String>getSsnByName(@PathVariable Integer ssn){
 	
